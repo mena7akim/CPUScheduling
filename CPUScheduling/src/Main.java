06ba3f850b94d6948a07dcc94a9be545d04eb088
mena7akim <102185230+mena7akim@users.noreply.github.com>
2023-12-11 11:50:09 +0200

    Modified AGScheduler, AGProcess and added comparator classes and AGReadyQueue class

--- CPUScheduling/src/Main.java
+++ CPUScheduling/src/Main.java
@@ -1,4 +1,9 @@
+import com.sun.source.tree.Tree;
+
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Random;
+import java.util.TreeSet;
 
 interface Scheduler{
     void addProcesses(Process[] processes);
@@ -66,15 +71,30 @@ class AGProcess extends Process{
 
     private int quantumTime;
 
+    private int queueTime = 0;
+
     String getQuantumTimeHistory() {
         return null;
     }
 
+    public AGProcess(String name, int burstTime, int arrivalTime, int priority){
+        super(name, burstTime, arrivalTime, priority);
+        this.AGFactor = calcAGFactor();
+    }
     public AGProcess(String name, int burstTime, int arrivalTime, int priority, int quantumTime){
         super(name, burstTime, arrivalTime, priority);
         this.quantumTime = quantumTime;
         this.AGFactor = calcAGFactor();
     }
+
+    public int getQueueTime(){
+        return queueTime;
+    }
+
+    public void setQueueTime(int queueTime){
+        this.queueTime = queueTime;
+    }
+
     public int getAGFactor() {
         return AGFactor;
     }
@@ -100,7 +120,7 @@ class AGProcess extends Process{
             return RFValue + this.arrivalTime + this.burstTime;
         }
         else if(RFValue > 10){
-            return 10 - this.arrivalTime + this.burstTime;
+            return 10 + this.arrivalTime + this.burstTime;
         }
         else{
             return priority + this.arrivalTime + this.burstTime;
@@ -191,16 +211,155 @@ class PriorityScheduler implements Scheduler{
 
 }
 
+class AGReadyQueue{
+    private TreeSet<AGProcess> processQueueTime;
+    private TreeSet<AGProcess> processAGFactor;
+
+    int mean = 0;
+
+    public AGReadyQueue(){
+        processQueueTime = new TreeSet<>(new QueueTimeComparator());
+        processAGFactor = new TreeSet<>(new AGFactorComparator());
+    }
+
+    public void addProcess(AGProcess process){
+        processQueueTime.add(process);
+        processAGFactor.add(process);
+        mean += process.getQuantumTime();
+    }
+
+    public AGProcess removeMinAG(){
+        AGProcess process = processAGFactor.pollFirst();
+        processQueueTime.remove(process);
+        mean -= process.getQuantumTime();
+        return process;
+    }
+
+    public AGProcess getMinAG(){
+        return processAGFactor.first();
+    }
+
+    public AGProcess removeFirst() {
+        AGProcess process = processQueueTime.pollFirst();
+        processAGFactor.remove(process);
+        mean -= process.getQuantumTime();
+        return process;
+    }
+
+    public int getIncreasedQuantumTime(){
+        return (int) Math.ceil(1.0 * mean / processQueueTime.size() / 10);
+    }
+
+    public int size(){
+        return processQueueTime.size();
+    }
+
+    public boolean isEmpty(){
+        return processQueueTime.isEmpty() && processAGFactor.isEmpty();
+    }
+}
+
+class AGFactorComparator implements Comparator<AGProcess> {
+    @Override
+    public int compare(AGProcess o1, AGProcess o2) {
+        if(o1.getAGFactor() == o2.getAGFactor()){
+            return o1.getQueueTime() - o2.getQueueTime();
+        }
+        return o1.getAGFactor() - o2.getAGFactor();
+    }
+}
+
+class QueueTimeComparator implements Comparator<AGProcess> {
+    @Override
+    public int compare(AGProcess o1, AGProcess o2) {
+        if(o1.getQueueTime() == o2.getQueueTime()){
+            return o1.getAGFactor() - o2.getAGFactor();
+        }
+        return o1.getQueueTime() - o2.getQueueTime();
+    }
+}
+
 class AGScheduler implements Scheduler{
+    AGReadyQueue readyQueue;
+    AGProcess[] AGProcesses;
+
+    int quantumTime;
+
+    public AGScheduler(int quantumTime){
+        this.quantumTime = quantumTime;
+        readyQueue = new AGReadyQueue();
+    }
+    public AGScheduler(int quantumTime, Process[] processes){
+        this.quantumTime = quantumTime;
+        readyQueue = new AGReadyQueue();
+        addProcesses(processes);
+    }
+
     @Override
     public void addProcesses(Process[] processes) {
-        AGProcess[] AGProcesses = (AGProcess[]) processes;
-
+        this.AGProcesses = (AGProcess[]) processes;
+        Arrays.sort(AGProcesses, new Comparator<AGProcess>() {
+            @Override
+            public int compare(AGProcess o1, AGProcess o2) {
+                return o1.getArrivalTime() - o2.getArrivalTime();
+            }
+        });
+        for(AGProcess process : AGProcesses){
+            process.setQuantumTime(quantumTime);
+        }
     }
 
     @Override
     public void runScheduler() {
-
+        int time = 0;
+        int lastProcessIndex = 0;
+        int nProcesses = AGProcesses.length;
+        int processEnterTime = -1;
+        int processExitTime = -1;
+        AGProcess currentProcess = null;
+        while(lastProcessIndex != nProcesses || !readyQueue.isEmpty() || time <= processExitTime) {
+            while(lastProcessIndex < nProcesses && AGProcesses[lastProcessIndex].getArrivalTime() == time){
+                AGProcesses[lastProcessIndex].setQueueTime(time);
+                readyQueue.addProcess(AGProcesses[lastProcessIndex]);
+                lastProcessIndex++;
+            }
+            if(processExitTime == time && currentProcess != null){
+                currentProcess.setBurstTime(currentProcess.getBurstTime() - (processExitTime - processEnterTime));
+                if(currentProcess.getBurstTime() > 0){
+                    currentProcess.setQuantumTime(currentProcess.getQuantumTime() + readyQueue.getIncreasedQuantumTime());
+                    currentProcess.setQueueTime(time);
+                    readyQueue.addProcess(currentProcess);
+                }
+                currentProcess = null;
+            }
+            if(currentProcess == null){
+                if(!readyQueue.isEmpty()){
+                    currentProcess = readyQueue.removeFirst();
+                    processEnterTime = time;
+                    processExitTime = time + Math.min(currentProcess.getQuantumTime(), currentProcess.getBurstTime());
+                }
+            }
+            else{
+                if((currentProcess.getQuantumTime() + 1) / 2 <= time - processEnterTime){
+                    if(!readyQueue.isEmpty()){
+                        AGProcess minAGProcess = readyQueue.getMinAG();
+                        AGFactorComparator agFactorComparator = new AGFactorComparator();
+                        if(agFactorComparator.compare(currentProcess, minAGProcess) > 0){
+                            currentProcess.setBurstTime(currentProcess.getBurstTime() - (time - processEnterTime));
+                            currentProcess.setQuantumTime(2 * currentProcess.getQuantumTime() - time + processEnterTime);
+                            currentProcess.setQueueTime(time);
+                            readyQueue.removeMinAG();
+                            readyQueue.addProcess(currentProcess);
+                            currentProcess = minAGProcess;
+                            processEnterTime = time;
+                            processExitTime = time + Math.min(currentProcess.getQuantumTime(), currentProcess.getBurstTime());
+                        }
+                    }
+                }
+            }
+            System.out.println(time + " " + (currentProcess == null ? "null" : currentProcess.getName()));
+            time++;
+        }
     }
 
     @Override
@@ -221,6 +380,15 @@ class AGScheduler implements Scheduler{
 }
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Helloooooooooooooo world!");
+        AGProcess[] processes = new AGProcess[4];
+        processes[0] = new AGProcess("P1", 17, 0, 4);
+        processes[1] = new AGProcess("P2", 6, 3, 9);
+        processes[2] = new AGProcess("P3", 10, 4, 3);
+        processes[3] = new AGProcess("P4", 4, 29, 8);
+        for(AGProcess process : processes){
+            System.out.println(process.getName() + " " + process.getAGFactor());
+        }
+        AGScheduler scheduler = new AGScheduler(4, processes);
+        scheduler.runScheduler();
     }
 }
